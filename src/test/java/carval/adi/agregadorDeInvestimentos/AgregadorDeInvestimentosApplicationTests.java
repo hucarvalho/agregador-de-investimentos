9352abc2026843ef92f48a64a8c06e9d9aab449b
hugo <carvalhohugo425@gmail.com>
2024-07-17 02:47:21 -0300

    first commit

---
+++ src/test/java/carval/adi/agregadorDeInvestimentos/AgregadorDeInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package carval.adi.agregadorDeInvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorDeInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
